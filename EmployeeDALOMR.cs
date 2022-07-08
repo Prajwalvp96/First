89d65fd4e95ac0397bc09a5604ad0076165562a6
Prajwalvp96 <85117573+Prajwalvp96@users.noreply.github.com>
2022-07-08 15:43:59 +0530

    Update EmpRepository
    Repository updated by Prajwal Patil

--- EmployeeDALOMR.cs
+++ EmployeeDALOMR.cs
@@ -6,7 +6,7 @@ using System.Web;
 using OMRWebApp.Models;
 using System.Data.Entity;
 using MySql.Data.EntityFramework;
-
+//ORM Entity Framework EmpDAL lib
 namespace OMRWebApp.DAL
 {
     [DbConfigurationType(typeof(MySqlEFConfiguration))]
